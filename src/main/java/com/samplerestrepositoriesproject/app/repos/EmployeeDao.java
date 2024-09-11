c95b549291850e6e19e695f788464147c11864dc
balajishiva2001 <balajishiva2001@gmail.com>
2024-09-11 19:39:42 +0530

    Made changes

--- src/main/java/com/samplerestrepositoriesproject/app/repos/EmployeeDao.java
+++ src/main/java/com/samplerestrepositoriesproject/app/repos/EmployeeDao.java
@@ -1,11 +1,14 @@
 package com.samplerestrepositoriesproject.app.repos;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.samplerestrepositoriesproject.app.entities.EmployeeEntity;
 
-@Repository
-public interface EmployeeDao extends JpaRepository<EmployeeEntity, Integer>{
-
+@RepositoryRestResource(path = "employees")
+public interface EmployeeDao extends JpaRepository<EmployeeEntity, Integer> {
+	@RestResource(path = "byUserName")
+	EmployeeEntity findByEmpName(@Param("empName") String empName);
 }
